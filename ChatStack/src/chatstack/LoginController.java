8e9a0de554835ba33cf38c6d92e4458a3b650db4
Hesham-Desktop <Hesham-Desktop@DESKTOP-K6QB0N3>
2018-04-26 16:29:27 +0200

    Login updated

--- ChatStack/src/chatstack/LoginController.java
+++ ChatStack/src/chatstack/LoginController.java
@@ -2,14 +2,19 @@
 package chatstack;
 
 
+
+import javafx.scene.input.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  *
@@ -18,22 +23,53 @@ import javafx.scene.image.ImageView;
 public class LoginController implements Initializable {
     
     @FXML
-    private Label label;
+    private Button maxIcon;
+    @FXML
+    private Button xIcon;
+    @FXML
+    private Button minIcon;
     
     
-    
+    double x,y;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    void dragToolBar(MouseEvent event) {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+    }
+
+    @FXML
+    void pressToolBar(MouseEvent event) {
+        x = event.getSceneX();
+        y = event.getSceneY();
     }
     
+        @FXML
+    void close(MouseEvent event) {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.close();
+    }
+
+    @FXML
+    void maximize(MouseEvent event) {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setFullScreenExitHint(" ");
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    void minimize(MouseEvent event) {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
 
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-    }    
+    }  
+    
+    
     
 }
