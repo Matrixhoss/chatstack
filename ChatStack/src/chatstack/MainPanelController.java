8aa70bc8d27d347f1e992cff41b99c460edb804b
Hesham-Desktop <Hesham-Desktop@DESKTOP-K6QB0N3>
2018-04-30 16:56:29 +0200

    MainPanel created

--- ChatStack/src/chatstack/MainPanelController.java
+++ ChatStack/src/chatstack/MainPanelController.java
@@ -1,54 +1,61 @@
-
 package chatstack;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.collections.FXCollections;
+import javafx.collections.*;
+import javafx.fxml.*;
 import javafx.scene.Node;
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-
 public class MainPanelController implements Initializable {
 
+    ObservableList<String> items = FXCollections.observableArrayList("test1", "test2");
+    ListView<String> list = new ListView<>(items);
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    
-    }    
-    
+        
+    }
+
     //<editor-fold defaultstate="collapsed" desc="TitleBar code DO NOT EDIT">
     //Start of title bar code
-    double x,y;
+    double x, y;
+
     @FXML
     void dragToolBar(MouseEvent event) {
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setX(event.getScreenX() - x);
         stage.setY(event.getScreenY() - y);
     }
+
     @FXML
     void pressToolBar(MouseEvent event) {
         x = event.getSceneX();
         y = event.getSceneY();
     }
-        @FXML
+
+    @FXML
     void close(MouseEvent event) {
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            stage.close();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
     }
+
     @FXML
     void maximize(MouseEvent event) {
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setFullScreenExitHint(" ");
         stage.setFullScreen(true);
     }
+
     @FXML
     void minimize(MouseEvent event) {
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setIconified(true);
     }
     //end of title bar code
 //</editor-fold>
-    
+
 }
