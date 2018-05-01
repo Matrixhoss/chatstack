9fdaa419fd714a05e1040f86d5b11709d7520cd7
Hesham-Desktop <Hesham-Desktop@DESKTOP-K6QB0N3>
2018-05-01 16:45:37 +0200

    gui updated

--- ChatStack/src/chatstack/GroupChatController.java
+++ ChatStack/src/chatstack/GroupChatController.java
@@ -10,9 +10,12 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 
 public class GroupChatController implements Initializable {
@@ -33,5 +36,42 @@ public class GroupChatController implements Initializable {
         test.getChildren().add(x);
     
     } 
+    //<editor-fold defaultstate="collapsed" desc="TitleBar code DO NOT EDIT">
+    //Start of title bar code
+    double x, y;
+
+    @FXML
+    void dragToolBar(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+    }
+
+    @FXML
+    void pressToolBar(MouseEvent event) {
+        x = event.getSceneX();
+        y = event.getSceneY();
+    }
+
+    @FXML
+    void close(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void maximize(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setFullScreenExitHint(" ");
+        stage.setFullScreen(true);
+    }
+
+    @FXML
+    void minimize(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+    }
+    //end of title bar code
+//</editor-fold>
     
 }
